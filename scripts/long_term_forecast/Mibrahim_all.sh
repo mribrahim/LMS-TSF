91d0b57c393579d8bcf896ebe98ec62840d3bc40
mribrahim <ibrahimdelibasoglu@gmail.com>
2024-09-19 16:40:18 +0200

    initial commit

---
+++ scripts/long_term_forecast/Mibrahim_all.sh
@@ -0,0 +1,4 @@
+sh ./scripts/long_term_forecast/ECL_script/Mibrahim.sh
+sh ./scripts/long_term_forecast/Traffic_script/Mibrahim.sh
+sh ./scripts/long_term_forecast/Exchange_script/Mibrahim.sh
+sh ./scripts/long_term_forecast/Weather_script/Mibrahim.sh
